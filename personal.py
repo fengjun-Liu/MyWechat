8e047bcf0347a8e00595cadb795a9a8743410b5c
liufengjun <939643949@qq.com>
2020-05-07 10:34:42 +0800

    删除私人信息

--- personal.py
+++
@@ -1,7 +0,0 @@
-wchaturl = "https://sc.ftqq.com/SCU88589T51d420a21b4ff294fe0ac4673bf201235e65c03803268.send"
-mail_host="smtp.163.com"  #设置服务器
-mail_user="alice611400@163.com"    #用户名
-mail_pass="liufengjun199122"   #口令 
-	 	 
-sender = 'alice611400@163.com'
-receivers = ['939643949@qq.com','zhangyan03@raisecom.com','fengningning@raisecom.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
